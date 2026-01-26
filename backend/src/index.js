ce08304f475a67b2b0bc71d942e80c4cef92317f
Nayan Chowdhury <nayan.1aacl@gmail.com>
2026-01-27 00:52:37 +0600

    Ready for Deployment

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,16 +1,31 @@
 import express from "express";
 import dotenv from "dotenv";
+import { connectDB } from "../lib/db.js";
+import path from "path";
 import authRoutes from "../routes/auth.route.js";
 import messageRoutes from "../routes/message.route.js";
-import { connectDB } from "../lib/db.js";
 
 dotenv.config();
 
 const app = express();
+const __dirname = path.resolve();
+
 const port = process.env.PORT || 5001;
 
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
+
+// Make ready for deployment
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.use((req, res, next) => {
+    if (req.path.startsWith("/api/")) {
+      return next();
+    }
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
