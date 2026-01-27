bfca32bab1f90143cf8ec12bc46cf24300c78a17
Nayan Chowdhury <nayan.1aacl@gmail.com>
2026-01-27 20:36:32 +0600

    Signup created

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,9 +1,59 @@
-export const signup = (req, res) => {
-  res.send("Signup Route");
+import { generateToken } from "../lib/utils.js";
+import User from "../models/User.js";
+import bcrypt from "bcryptjs";
+
+export const signup = async (req, res) => {
+  const { fullName, email, password } = req.body;
+
+  try {
+    if (!fullName || !email || !password) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ message: "Password must be at least 6 characters long" });
+    }
+
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    if (!emailRegex.test(email)) {
+      return res.status(400).json({ message: "Invalid email format" });
+    }
+
+    const user = await User.findOne({ email });
+    if (user) {
+      return res.status(400).json({ message: "Email already in use" });
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new User({ fullName, email, password: hashedPassword });
+
+    if (newUser) {
+      generateToken(newUser._id, res);
+      await newUser.save();
+
+      return res.status(201).json({
+        _id: newUser._id,
+        fullName: newUser.fullName,
+        email: newUser.email,
+        profilePic: newUser.profilePic,
+      });
+
+      //TODO: Send welcome email
+    } else {
+      res.status(400).json({ message: "User creation failed" });
+    }
+  } catch (error) {
+    console.log("Error in Signup:", error);
+    res.status(500).json({ message: "Internal Server error" });
+  }
 };
 
 export const login = (req, res) => {
-  res.send("Login Route");
+  res.send("Loginbbbbbb Route");
 };
 
 export const logout = (req, res) => {
--- backend/lib/db.js
+++ backend/lib/db.js
@@ -6,5 +6,6 @@ export const connectDB = async () => {
     console.log("Database connected successfully");
   } catch (error) {
     console.error("Database connection error:", error);
+    process.exit(1); // Exit process with failure
   }
 };
