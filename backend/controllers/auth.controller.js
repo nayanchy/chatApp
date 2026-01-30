e6fe375f2d077bf634bce2ef90a8406ac17bc9e9
Nayan Chowdhury <nayan.1aacl@gmail.com>
2026-01-30 12:13:55 +0600

    Resend Email Setup with SignUp

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,6 +1,8 @@
 import { generateToken } from "../lib/utils.js";
 import User from "../models/User.js";
 import bcrypt from "bcryptjs";
+import { sendWelcomeEmail } from "../src/emails/emailHandlers.js";
+import dotenv from "dotenv";
 
 export const signup = async (req, res) => {
   const { fullName, email, password } = req.body;
@@ -31,21 +33,26 @@ export const signup = async (req, res) => {
 
     const newUser = new User({ fullName, email, password: hashedPassword });
 
-    if (newUser) {
-      generateToken(newUser._id, res);
-      await newUser.save();
+    const savedUser = await newUser.save();
+    generateToken(savedUser._id, res);
 
-      return res.status(201).json({
-        _id: newUser._id,
-        fullName: newUser.fullName,
-        email: newUser.email,
-        profilePic: newUser.profilePic,
-      });
-
-      //TODO: Send welcome email
-    } else {
-      res.status(400).json({ message: "User creation failed" });
+    try {
+      await sendWelcomeEmail(
+        savedUser.email,
+        savedUser.fullName,
+        process.env.CLIENT_URL,
+      );
+      console.log("Welcome email sent successfully");
+    } catch (error) {
+      console.error("Error sending welcome email:", error);
     }
+
+    return res.status(201).json({
+      _id: newUser._id,
+      fullName: newUser.fullName,
+      email: newUser.email,
+      profilePic: newUser.profilePic,
+    });
   } catch (error) {
     console.log("Error in Signup:", error);
     res.status(500).json({ message: "Internal Server error" });
